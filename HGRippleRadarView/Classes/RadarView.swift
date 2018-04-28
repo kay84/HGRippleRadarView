10f81f97a3ff7b83e944c8cdad394155dae48e0e
H. Eren ÇELİK <eren.celik@icloud.com>
2018-04-28 21:38:43 +0300

    RippleView and RadarView classes are merged as RadarView for usability

--- HGRippleRadarView/Classes/RadarView.swift
+++ HGRippleRadarView/Classes/RadarView.swift
@@ -2,18 +2,17 @@
 //  RadarView.swift
 //  HGNearbyUsers_Example
 //
-//  Created by Hamza Ghazouani on 25/01/2018.
+//  Created by Hamza Ghazouani on 23/01/2018.
 //  Copyright © 2018 CocoaPods. All rights reserved.
 //
 
 import UIKit
 
 
-/// A radar view with ripple animation
+/// A view with ripple animation
+
 @IBDesignable
-final public class RadarView: RippleView {
-    
-    // MARK: public properties
+public final class RadarView: UIView {
     
     /// the maximum number of items that can be shown in the radar view, if you use more, some layers will overlaying other layers
     public var radarCapacity: Int {
@@ -87,21 +86,268 @@ final public class RadarView: RippleView {
         }
     }
     
+    // MARK: Private Properties
     
-    // MARK: View Life Cycle
+    /// the center circle used for the scale animation
+    private var centerAnimatedLayer: CAShapeLayer!
     
-    override func setup() {
+    /// the center disk point
+    private var diskLayer: CAShapeLayer!
+    
+    /// The duration to animate the central disk
+    private var centerAnimationDuration: CFTimeInterval {
+        return CFTimeInterval(animationDuration) * 0.90
+    }
+    
+    /// The duration to animate one circle
+    private var circleAnimationDuration: CFTimeInterval {
+        if circleLayers.count ==  0 {
+            return CFTimeInterval(animationDuration)
+        }
+        return CFTimeInterval(animationDuration) / CFTimeInterval(circleLayers.count)
+    }
+    
+    /// The timer used to start / stop circles animation
+    private var circlesAnimationTimer: Timer?
+    
+    /// The timer used to start / stop disk animation
+    private var diskAnimationTimer: Timer?
+    
+    // MARK: Internal properties
+    
+    /// The maximum possible radius of circle
+    var maxCircleRadius: CGFloat {
+        if numberOfCircles == 0 {
+            return min(bounds.midX, bounds.midY)
+        }
+        return (circlesPadding * CGFloat(numberOfCircles - 1) + minimumCircleRadius)
+    }
+    
+    /// the circles surrounding the disk
+    var circleLayers = [CAShapeLayer]()
+    var circles = [Circle]()
+    
+    /// The padding between circles
+    var circlesPadding: CGFloat {
+        if paddingBetweenCircles != -1 {
+            return paddingBetweenCircles
+        }
+        let availableRadius = min(bounds.width, bounds.height)/2 - (minimumCircleRadius)
+        return  availableRadius / CGFloat(numberOfCircles)
+    }
+    
+    // MARK: Public Properties
+    
+    @IBInspectable public var minDistance: Double = 0 {
+        didSet {
+            redrawDisks()
+            redrawCircles()
+        }
+    }
+    
+    @IBInspectable public var maxDistance: Double = 20000 {
+        didSet {
+            redrawDisks()
+            redrawCircles()
+        }
+    }
+    
+    /// The radius of the disk in the view center, the default value is 5
+    @IBInspectable public var diskRadius: CGFloat = 5 {
+        didSet {
+            redrawDisks()
+            redrawCircles()
+        }
+    }
+    
+    /// The color of the disk in the view center, the default value is ripplePink color
+    @IBInspectable public var diskColor: UIColor = .ripplePink {
+        didSet {
+            diskLayer.fillColor = diskColor.cgColor
+            centerAnimatedLayer.fillColor = diskColor.cgColor
+        }
+    }
+    
+    /// The number of circles to draw around the disk, the default value is 3, if the forcedMaximumCircleRadius is used the number of drawn circles could be less than numberOfCircles
+    @IBInspectable public var numberOfCircles: Int = 3 {
+        didSet {
+            redrawCircles()
+        }
+    }
+    
+    /// The padding between circles
+    @IBInspectable public var paddingBetweenCircles: CGFloat = -1 {
+        didSet {
+            redrawCircles()
+        }
+    }
+    
+    /// The color of the off status of the circle, used for animation
+    @IBInspectable public var circleOffColor: UIColor = .rippleDark {
+        didSet {
+            circleLayers.forEach {
+                $0.strokeColor = circleOffColor.cgColor
+            }
+        }
+    }
+    
+    /// The color of the on status of the circle, used for animation
+    @IBInspectable public var circleOnColor: UIColor = .rippleWhite
+    
+    /// The minimum radius of circles, used to make space between the disk and the first circle, the radius must be grather than 5px , because if not the first circle will not be shown, the default value is 10, it's recommanded to use a value grather than the disk radius if you would like to show circles outside disk
+    @IBInspectable public var minimumCircleRadius: CGFloat = 10 {
+        didSet {
+            if minimumCircleRadius < 5 {
+                minimumCircleRadius = 5
+            }
+            redrawCircles()
+        }
+    }
+    
+    /// The duration of the animation, the default value is 0.9
+    @IBInspectable public var animationDuration: CGFloat = 0.9 {
+        didSet {
+            stopAnimation()
+            startAnimation()
+        }
+    }
+    
+    
+    // MARK: init methods
+    
+    /// Initializes and returns a newly allocated view object with the specified frame rectangle.
+    ///
+    /// - Parameter frame: The frame rectangle for the view, measured in points.
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    /// Initializes and returns a newly allocated view object from data in a given unarchiver.
+    ///
+    /// - Parameter aDecoder: An unarchiver object.
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
         paddingBetweenCircles = 40
         let viewRadius = min(bounds.midX, bounds.midY)
         minimumCircleRadius = viewRadius > 120 ? 60 : diskRadius + 15
-        
-        super.setup()
+        drawSublayers()
+        animateSublayers()
     }
     
-    override func redrawCircles(with itemRadius: CGFloat, paddingBetweenItems: CGFloat) {
-        super.redrawCircles(with: self.itemRadius, paddingBetweenItems: self.paddingBetweenItems)
-        redrawItems()
+    // MARK: Drawing methods
+    
+    /// Calculate the radius of a circle by using its index
+    ///
+    /// - Parameter index: the index of the circle
+    /// - Returns: the radius of the circle
+    func radiusOfCircle(at index: Int) -> CGFloat {
+        return (circlesPadding * CGFloat(index)) + minimumCircleRadius
     }
+    
+    /// Lays out subviews.
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+        diskLayer.position = bounds.center
+        centerAnimatedLayer.position = bounds.center
+        circleLayers.forEach {
+            $0.position = bounds.center
+        }
+    }
+    
+    /// Draws disks and circles
+    private func drawSublayers() {
+       drawDisks()
+       redrawCircles()
+    }
+    
+    /// Draw central disk and the disk for the central animation
+    private func drawDisks() {
+        diskLayer = Drawer.diskLayer(radius: diskRadius, origin: bounds.center, color: diskColor.cgColor)
+        layer.insertSublayer(diskLayer, at: 0)
+        
+        centerAnimatedLayer = Drawer.diskLayer(radius: diskRadius, origin: bounds.center, color: diskColor.cgColor)
+        centerAnimatedLayer.opacity = 0.3
+        layer.addSublayer(centerAnimatedLayer)
+    }
+    
+    /// Redraws disks by deleting the old ones and drawing a new ones, called for example when the radius changed
+    private func redrawDisks() {
+        diskLayer.removeFromSuperlayer()
+        centerAnimatedLayer.removeFromSuperlayer()
+        
+        drawDisks()
+    }
+
+    /// Redraws circles by deleting old ones and drawing new ones, this method is called, for example, when the number of circles changed
+    func redrawCircles() {
+        circleLayers.forEach {
+            $0.removeFromSuperlayer()
+        }
+        circleLayers.removeAll()
+        circles.removeAll()
+        for i in 0 ..< numberOfCircles {
+            drawCircle(with: i)
+        }
+    }
+    
+    /// Draws the circle by using the index to calculate the radius
+    ///
+    /// - Parameter index: the index of the circle
+    private func drawCircle(with index: Int) {
+        let distanceInterval: Double = (maxDistance - minDistance) / Double(numberOfCircles)
+        let minDistanceForCircle = index == 0 ? minDistance : Double(index) * distanceInterval
+        let maxDistanceForCircle = index == numberOfCircles - 1 ? maxDistance : Double(index + 1) * distanceInterval
+        let radius = radiusOfCircle(at: index)
+        if radius > maxCircleRadius { return }
+        let origin = bounds.center
+        let circleLayer = Drawer.circleLayer(radius: radius, origin: origin, color: circleOffColor.cgColor)
+        circleLayer.lineWidth = 2.0
+        circleLayers.append(circleLayer)
+        self.layer.addSublayer(circleLayer)
+        circles.append(Circle(name: "C\(index + 1)", itemRadius: itemRadius, paddingBetweenItems: paddingBetweenItems, origin: origin, radius: radius, distanceRange: (minDistanceForCircle...maxDistanceForCircle)))
+    }
+    
+    // MARK: Animation methods
+    
+    /// Add animation to central disk and the surrounding circles 
+    private func animateSublayers() {
+        animateCentralDisk()
+        animateCircles()
+        startAnimation()
+    }
+    
+    /// Animates the central disk by changing the opacitiy and the scale
+    @objc private func animateCentralDisk() {
+        let maxScale = maxCircleRadius / diskRadius
+        let scaleAnimation = Animation.transform(to: maxScale)
+        let alphaAnimation = Animation.opacity(from: 0.3, to: 0.0)
+        let groupAnimation = Animation.group(animations: scaleAnimation, alphaAnimation, duration: centerAnimationDuration)
+        centerAnimatedLayer.add(groupAnimation, forKey: nil)
+        self.layer.addSublayer(centerAnimatedLayer)
+    }
+    
+    /// Animates circles by changing color from off to on color
+    @objc private func animateCircles() {
+        for index in 0 ..< circleLayers.count {
+            let colorAnimation = Animation.color(from: circleOffColor.cgColor, to: circleOnColor.cgColor)
+            colorAnimation.duration = circleAnimationDuration
+            colorAnimation.autoreverses = true
+            colorAnimation.beginTime = CACurrentMediaTime() + CFTimeInterval(circleAnimationDuration * Double(index))
+            circleLayers[index].add(colorAnimation, forKey: "strokeColor")
+        }
+    }
+}
+
+extension RadarView {
+    
+    
+    // MARK: View Life Cycle
     
     private func redrawItems() {
         circles.forEach { circle in
@@ -217,6 +463,7 @@ final public class RadarView: RippleView {
             currentItemView = nil
         }
     }
+    
 }
 
 extension RadarView: CAAnimationDelegate {
@@ -234,6 +481,24 @@ extension RadarView: CAAnimationDelegate {
 
 // MARK: public methods
 extension RadarView {
+    
+    /// Start the ripple animation
+    public func startAnimation() {
+        layer.removeAllAnimations()
+        circlesAnimationTimer?.invalidate()
+        diskAnimationTimer?.invalidate()
+        let timeInterval = CFTimeInterval(animationDuration) + circleAnimationDuration
+        circlesAnimationTimer =  Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(animateCircles), userInfo: nil, repeats: true)
+        diskAnimationTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(animateCentralDisk), userInfo: nil, repeats: true)
+    }
+    
+    /// Stop the ripple animation
+    public func stopAnimation() {
+        layer.removeAllAnimations()
+        circlesAnimationTimer?.invalidate()
+        diskAnimationTimer?.invalidate()
+    }
+    
     /// Add a list of items to the radar view
     ///
     /// - Parameters:
@@ -295,4 +560,13 @@ extension Drawer {
         view.clipsToBounds = true
         view.backgroundColor = color
         return view
-    }}
+    }
+    
+}
+
+
+
+
+
+
+
